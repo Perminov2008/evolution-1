af3d90f3d74748ec75908183f5ae764af27ef212
ASUS <perminov10992@gmail.com>
2023-01-03 00:55:14 +0300

    Сделал отрисовку ботов яда и энергии. Уменьшил размеры поля.

--- Evolution.py
+++ Evolution.py
@@ -1,15 +1,22 @@
 import pygame
+import random
+
+# Поле получилось 20 на 15 (Вы просили уменьшить)
+
 WIDTH = 1000
-HEIGHT = 600
-FPS = 30
-pole_x = 60
-pole_y = 30
-move = 25
+HEIGHT = 750
+FPS = 10
+pole_x = 20
+pole_y = 15
+move = 50
 
 white = (255, 255, 255)
+gray = (125, 125, 125)
 black = (0, 0, 0)
 red = (255, 0, 0)
+yellow = (225, 225, 0)
 blue = (0, 0, 255)
+green = (0, 150, 75)
 
 pygame.init()
 pygame.mixer.init()
@@ -25,6 +32,11 @@ while running:
             running = False
     x = 0
     y = 0
+    x1 = -45
+    y1 = -45
+    x2 = -5
+    y2 = -5
+    screen.fill(green)
     pygame.draw.rect(screen, blue, (0, 0, WIDTH, HEIGHT), 2)
     for i in range(pole_x):
         x += move
@@ -33,18 +45,43 @@ while running:
         y += move
         pygame.draw.line(screen, blue, [0, y], [WIDTH, y], 4)
     # pygame.draw.rect(screen, white, (x1, y1, x2, y2))
-    x1 = -20
-    y1 = -20
-    x2 = -5
-    y2 = -5
     for i in range(pole_y):
         y1 += move
         y2 += move
-        x1 = -20
+        x1 = -45
         x2 = -5
         for j in range(pole_x):
             x1 += move
             x2 += move
-            pygame.draw.polygon(screen, white, [[x1, y1], [x1, y2], [x2, y2], [x2, y1]])
+            energy = random.randint(10, 100) # Пока что рандомное значение из-за этого на экране часто меняются цвета
+            if i < pole_y / 2:  # Здесь должно быть условие на то что в этой клетке бот
+                if energy <= 25:
+                    pygame.draw.polygon(screen, white, [[x1, y1], [x1, y2], [x2, y2], [x2, y1]])
+                elif energy <= 75:
+                    pygame.draw.polygon(screen, yellow, [[x1, y1], [x1, y2], [x2, y2], [x2, y1]])
+                else:
+                    pygame.draw.polygon(screen, red, [[x1, y1], [x1, y2], [x2, y2], [x2, y1]])
+                pygame.draw.circle(screen, gray, ((x1 + x2) / 2 + 0.25, (y1 + y2) / 2), 15)
+                pygame.draw.polygon(screen, gray,
+                                    [[x1 + 5, y1 + 20], [x1 + 5, y2 - 5], [x2 - 5, y2 - 5], [x2 - 5, y1 + 20]])
+                pygame.draw.circle(screen, gray, ((x1 + x2) / 2 + 0.25, (y1 + y2) / 2), 15)
+                pygame.draw.circle(screen, red, ((x1 + x2) / 2 + 10, (y1 + y2) / 2 - 2), 4)
+                pygame.draw.circle(screen, red, ((x1 + x2) / 2 - 10, (y1 + y2) / 2 - 2), 4)
+                pygame.draw.polygon(screen, black,
+                                    [[x1 + 10, y1 + 27], [x1 + 10, y2 - 10], [x2 - 10, y2 - 10], [x2 - 10, y1 + 27]])
+            else: # Здесь должно быть условие на то что в этой клетке яд
+                pygame.draw.polygon(screen, white, [[x1, y1], [x1, y2], [x2, y2], [x2, y1]])
+                pygame.draw.polygon(screen, green,
+                                    [[x1 + 10, y1 + 5], [x1 + 10, y2 - 30], [x2 - 10, y2 - 30], [x2 - 10, y1 + 5]])
+                pygame.draw.polygon(screen, green,
+                                    [[x1 + 15, y1 + 10], [x1 + 15, y2 - 25], [x2 - 15, y2 - 25], [x2 - 15, y1 + 10]])
+                pygame.draw.polygon(screen, green,
+                                    [[x1 + 5, y1 + 15], [x1 + 5, y2 - 5], [x2 - 5, y2 - 5], [x2 - 5, y1 + 15]])
+                pygame.draw.circle(screen, black, ((x1 + x2) / 2 + 8, (y1 + y2) / 2), 4)
+                pygame.draw.circle(screen, black, ((x1 + x2) / 2 - 8, (y1 + y2) / 2), 4)
+                pygame.draw.polygon(screen, black,
+                                    [[x1 + 10, y1 + 27], [x1 + 10, y2 - 10], [x2 - 10, y2 - 10]])
+                pygame.draw.polygon(screen, red,
+                                    [[x1 + 10, y1 + 5], [x1 + 10, y2 - 33], [x2 - 10, y2 - 33], [x2 - 10, y1 + 5]])
     pygame.display.flip()
 pygame.quit()
